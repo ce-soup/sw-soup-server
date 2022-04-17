6d98305bebdf386e831d05043f5ffc9000d68120
himitery <himit0131@gmail.com>
2022-04-17 20:47:03 +0900

    feat(member): member controller description 추가

--- src/module/member/controller/member.controller.ts
+++ src/module/member/controller/member.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Response } from 'express';
 
 import { RoleGuard } from '@/module/auth/role-guard.decorator';
 import { AuthUser } from '@/module/auth/auth-user.decorators';
@@ -6,13 +8,16 @@ import { AuthUserResponse, Role } from '@/module/auth/dto/response/AuthUserRespo
 import { MemberService } from '@/module/member/services/member.service';
 import { MemberResponse } from '@/module/member/dto/response/member.response';
 
+@ApiTags('MemberController')
 @Controller('/api/v1/member')
 @RoleGuard([Role.General])
 export class MemberController {
   constructor(private readonly memberService: MemberService) {}
 
   @Get('/me')
-  async getMe(@AuthUser() user: AuthUserResponse): Promise<MemberResponse> {
-    return this.memberService.findMemberById(user.memberId);
+  @ApiOperation({ summary: 'GetMe', description: '자기 자신에 대한 정보를 가져올 수 있어요.' })
+  @ApiOkResponse({ description: 'OK', type: MemberResponse })
+  async getMe(@Res() res: Response, @AuthUser() user: AuthUserResponse): Promise<MemberResponse> {
+    return await this.memberService.findMemberById(user.memberId);
   }
 }
