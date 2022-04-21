3ee263f34ed61db15e16f2e47fccf1ae7ad88432
himitery <himit0131@gmail.com>
2022-04-22 01:33:44 +0900

    refactor(grpc): grpc setup 리팩토링

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import '@/config/env';
 import { join } from 'path';
 
 import { NestFactory } from '@nestjs/core';
-import { GrpcOptions, Transport } from '@nestjs/microservices';
+import { GrpcOptions } from '@nestjs/microservices';
 import { SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
@@ -10,29 +10,23 @@ import { eurekaClientStart } from '@/common/eureka-client';
 import { swaggerDocumentOptions, swaggerOptions } from '@/common/swagger';
 import { AuthGuard } from '@/module/auth/auth.guard';
 import { ValidationPipe } from './common/validation-pipe.config';
+import { grpcOptions } from '@/common/grpc';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: true,
   });
+
+  // cors
   app.enableCors();
 
+  // swagger
   SwaggerModule.setup('swagger', app, SwaggerModule.createDocument(app, swaggerOptions), swaggerDocumentOptions);
 
-  const protoDir = join(__dirname, '..', 'proto');
-  app.connectMicroservice<GrpcOptions>({
-    transport: Transport.GRPC,
-    options: {
-      url: `${process.env.NODE_ENV === 'production' ? 'soup' : 'localhost'}:${+(process.env.GRPC_PORT ?? '5000')}`,
-      package: 'soup',
-      protoPath: [join(protoDir, 'member.proto')],
-      loader: {
-        longs: Number,
-        defaults: true,
-      },
-    },
-  });
+  // grpc
+  app.connectMicroservice<GrpcOptions>(grpcOptions(join(__dirname, '..', 'proto')));
 
+  // eureka
   eurekaClientStart();
 
   await app.startAllMicroservices();
