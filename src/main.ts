03b4319e3dc7acd1c5d3bdc0023de7357c98a2b6
himitery <himit0131@gmail.com>
2022-04-22 01:31:05 +0900

    refactor(swagger): swagger setup 리팩토링

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import { join } from 'path';
 
 import { NestFactory } from '@nestjs/core';
 import { GrpcOptions, Transport } from '@nestjs/microservices';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { eurekaClientStart } from '@/common/eureka-client';
+import { swaggerDocumentOptions, swaggerOptions } from '@/common/swagger';
 import { AuthGuard } from '@/module/auth/auth.guard';
 import { ValidationPipe } from './common/validation-pipe.config';
 
@@ -16,36 +17,7 @@ async function bootstrap() {
   });
   app.enableCors();
 
-  SwaggerModule.setup(
-    'swagger',
-    app,
-    SwaggerModule.createDocument(
-      app,
-      new DocumentBuilder()
-        .setTitle('soup service server')
-        .setDescription('soup service server api documents')
-        .setVersion('1.0')
-        .addBasicAuth(
-          {
-            type: 'apiKey',
-            name: 'Authorization',
-            in: 'header',
-            scheme: 'Bearer',
-          },
-          'Authorization',
-        )
-        .addServer(
-          process.env.NODE_ENV === 'production' ? '133.186.144.24:3000' : `http://localhost:3000`,
-          'Inferred Url',
-        )
-        .build(),
-    ),
-    {
-      swaggerOptions: {
-        persistAuthorization: true,
-      },
-    },
-  );
+  SwaggerModule.setup('swagger', app, SwaggerModule.createDocument(app, swaggerOptions), swaggerDocumentOptions);
 
   const protoDir = join(__dirname, '..', 'proto');
   app.connectMicroservice<GrpcOptions>({
