6d39e5bce1a5e9fcddc66c6a2fb94a33c8d9ea90
himitery <himit0131@gmail.com>
2022-04-21 16:24:56 +0900

    feat(member): minio module에 provider 추가

--- src/module/minio/minio.module.ts
+++ src/module/minio/minio.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { MinioService } from '@/module/minio/services/minio.service';
 
 @Module({
+  providers: [MinioService],
   exports: [MinioService],
 })
 export class MinioModule {}
