b464813fb2cf1661f3efa9db9c164efd7bdbd001
himitery <himit0131@gmail.com>
2022-05-20 23:14:41 +0900

    feat(swagger): 카테고리 API 설명 추가

--- src/module/category/dto/request/create-category.request.ts
+++ src/module/category/dto/request/create-category.request.ts
@@ -1,9 +1,11 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export interface ICreateCategoryRequest {
   name: string;
   parentId?: string;
 }
 
 export class CreateCategoryRequest implements ICreateCategoryRequest {
-  name: string;
-  parentId: string;
+  @ApiProperty() name: string;
+  @ApiProperty() parentId: string;
 }
